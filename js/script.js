de54ff06a9a3b7af8ed9b1984a9e5078999cfe34
Noorain Manekia <noorainfmanekia@gmail.com>
2022-05-09 14:58:26 -0700

    Created variables for elements and added event listener to button

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const inputBox = document.querySelector(".letter");
+const wordInProgress = document.querySelector("word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingNumber = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholders = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholders(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = inputBox.value;
+    console.log(guess);
+    inputBox.value = "";
+});
